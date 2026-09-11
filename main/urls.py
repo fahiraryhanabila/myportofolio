cf97c3384a10435b2e8eb59d656ff7b8e6cc1926
fahiraryhanabila <fahiraryhanabila@ui.ac.id>
2026-09-11 21:41:53 +0700

    feat: add URL route for education page

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from main.views import show_main, show_experience
+from main.views import show_main
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
+    path("education/", show_main, name="show_education"),
 ]
