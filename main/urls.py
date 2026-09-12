5c7e0816dfcf1771048e21aae0e27d441b6be2a1
fahiraryhanabila <fahiraryhanabila@ui.ac.id>
2026-09-12 22:17:32 +0700

    feat: add URL route for data seeder
    Register seeder-data/ endpoint to trigger seeder_data view.

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from main.views import show_main, show_experience
-from main.views import show_main
+from main.views import show_main, show_experience, show_education, seeder_data
 
 app_name = "main"
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
-    path("education/", show_main, name="show_education"),
+    path("education/", show_education, name="show_education"),
+    path("seeder-data/", seeder_data, name="seeder_data"),
 ]
